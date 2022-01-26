2c3441c72664e601c2a48c9ad88441ed5db1d2c8
temarazin <temarazin@gmail.com>
2022-01-26 09:58:04 +0300

    feat: add autoload cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,33 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+/* Templates */
+const cardTemplate = document.querySelector('#card').content;
+
 /* popups */
 let popupProfile = document.querySelector('.popup');
 
@@ -13,6 +43,7 @@ let inputProfileProf = document.querySelector('.form-profile__input[name="profes
 /* elements */
 let profileName = document.querySelector('.profile__name');
 let profileProf = document.querySelector('.profile__profession');
+const cardContainer = document.querySelector('.photo-grid');
 
 /* functions */
 function openPopupProfile() {
@@ -32,7 +63,21 @@ function formProfileSubmit(evt) {
   closePopupProfile();
 }
 
+function addCard(data) {
+  let card = cardTemplate.cloneNode(true);
+  let image = card.querySelector('.photo-grid__image');
+  let name = card.querySelector('.photo-grid__item-name');
+
+  image.src = data.link;
+  name.textContent = data.name;
+
+  cardContainer.append(card);
+}
+
 /* events */
 btnEditProfile.addEventListener('click', openPopupProfile);
 btnCloseProfile.addEventListener('click', closePopupProfile);
 formProfile.addEventListener('submit', formProfileSubmit);
+
+
+initialCards.forEach( item => addCard(item) );
