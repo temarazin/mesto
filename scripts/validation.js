7386e63762a6692b00fb95e4f17b719174f0bb0f
temarazin <temarazin@gmail.com>
2022-02-02 02:20:02 +0300

    refactor: validation.js

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,37 +1,10 @@
-function setValidationListeners() {
-  const forms = Array.from(document.querySelectorAll('.form'));
-  forms.forEach( form => {
-    form.addEventListener('input', validate);
-  });
-}
-
-function validate(e) {
-  validateInput(e.target);
-  validateForm(e.currentTarget);
-}
-
-function validateInput(input) {
-  let result = input.validity.valid;
-  if (result) {
-    hideValidationError(input.id);
+function toggleButtonSubmitState(button, isEnabled) {
+  if (isEnabled) {
+    button.classList.remove('form__submit_disabled');
   } else {
-    showValidationError(input.id, input.validationMessage);
+    button.classList.add('form__submit_disabled');
   }
-  return result;
-}
-
-function validateForm(form) {
-  let result = true;
-  const btnSubmit = form.querySelector('.form__submit');
-  const inputList = form.querySelectorAll('.form__input');
-  Array.from(inputList).forEach( (input) => {
-    result &= isValid(input);
-  });
-  toggleButtonSubmitState(btnSubmit, !result);
-}
-
-function isValid(input) {
-  return input.validity.valid;
+  button.disabled = !isEnabled;
 }
 
 function showValidationError(fieldId, message) {
@@ -46,13 +19,40 @@ function hideValidationError(fieldId) {
   messageBox.classList.remove('form__error-msg_active');
 }
 
-function toggleButtonSubmitState(button, isDisabled) {
-  if (isDisabled) {
-    button.classList.add('form__submit_disabled');
+function isValid(input) {
+  return input.validity.valid;
+}
+
+function validateInput(input) {
+  let result = isValid(input);
+  if (result) {
+    hideValidationError(input.id);
   } else {
-    button.classList.remove('form__submit_disabled');
+    showValidationError(input.id, input.validationMessage);
   }
-  button.disabled = isDisabled;
+  return result;
+}
+
+function validateForm(form) {
+  let isValidForm = true;
+  const btnSubmit = form.querySelector('.form__submit');
+  const inputList = form.querySelectorAll('.form__input');
+  Array.from(inputList).forEach( (input) => {
+    isValidForm &= isValid(input);
+  });
+  toggleButtonSubmitState(btnSubmit, isValidForm);
+}
+
+function validate(e) {
+  validateInput(e.target);
+  validateForm(e.currentTarget);
+}
+
+function setValidationListeners() {
+  const forms = Array.from(document.querySelectorAll('.form'));
+  forms.forEach( form => {
+    form.addEventListener('input', validate);
+  });
 }
 
 function enableValidation() {
